4490116329744787b3ced6bd10b587b9617e196d
Hemang Rajyaguru <hemdagem@gmail.com>
2020-02-17 23:11:29 +0000

    added ability to check dynamo

--- index.ts
+++ index.ts
@@ -1,33 +1,45 @@
 import { SNS, config, DynamoDB } from 'aws-sdk'
+import { ItemList } from 'aws-sdk/clients/dynamodb';
 
 config.update({ region: 'eu-west-1' });
 
-test('Publish Sns topic', async () => {
+function getDynamoItem(tableName: string, key: string, item: string) {
+  var promise = new Promise<ItemList>(function (resolve, reject) {
+    window.setTimeout(async function () {
+      var dynamoParams = {
+        RequestItems: {
+          [tableName]: {
+            Keys: [
+              {
+                [key]: {
+                  S: item
+                }
+              }],
+          }
+        }
+      };
+      let dynamo = new DynamoDB();
+      let response = await dynamo.batchGetItem(dynamoParams).promise();
 
-  var params = {
-    Message: 'Test Message',
-    TopicArn: ''
-  };
+      resolve(response.Responses.Test);
+    }, 3000);
+  });
+  return promise;
+}
 
+test('Publish sns message and expect message to reach dynamo ', async () => {
 
   let sns = new SNS();
-  await sns.publish(params).promise();
+  let topics = await sns.listTopics().promise();
+  let topic = topics.Topics.find(p => p.TopicArn.indexOf(":Test") > -1);
 
-  var dynamoParams = {
-    RequestItems: {
-      "Test": {
-        Keys: [
-          {
-            "Test": {
-              S: "Hello"
-            }
-          }],
-      }
-    }
+  var params = {
+    Message: 'Dev',
+    TopicArn: topic.TopicArn
   };
+  await sns.publish(params).promise();
+  var dynamoCheck = await getDynamoItem("Test", "Test", params.Message);
 
-  let dynamo = new DynamoDB();
-  let response = await dynamo.batchGetItem(dynamoParams).promise();
+  expect(dynamoCheck[0].Test.S).toBe(params.Message);
 
-  console.log(response.Responses.Test);
 });
